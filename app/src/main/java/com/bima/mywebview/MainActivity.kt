19b93926445ca49ace54a81d2bb023f1bdfc9d57
bimamib <bimaprast1601@gmail.com>
2023-10-04 00:57:51 +0700

    melakukan konfigurasi pada webViewClient

--- app/src/main/java/com/bima/mywebview/MainActivity.kt
+++ app/src/main/java/com/bima/mywebview/MainActivity.kt
@@ -3,6 +3,8 @@ package com.bima.mywebview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +14,12 @@ class MainActivity : AppCompatActivity() {
 
         val webView = findViewById<WebView>(R.id.webView)
         webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                Toast.makeText(this@MainActivity, "Web Dicoding berhasil dimuat", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         webView.loadUrl("https://www.dicoding.com")
     }
